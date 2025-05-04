2f02932df29ce19625de748de059b4cf8167ef38
Özay Melih Yıldız <102221334+Ozaui@users.noreply.github.com>
2025-05-05 02:31:26 +0300

    footer

---
+++ Front-end/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import DefaultFooter from "./components/Footer/DefaultFooter";
+import LoggedInNavbar from "./components/Navbar/LoggedInNavbar";
+
+function App() {
+  return (
+    <>
+      <LoggedInNavbar />
+      <DefaultFooter />
+    </>
+  );
+}
+
+export default App;
---
+++ Front-end/src/components/Footer/DefaultFooter.tsx
@@ -0,0 +1,64 @@
+import { Box, Grid } from "@mui/material";
+import Logo from "../../assets/images/logo.png";
+import { Link } from "react-router-dom";
+
+function DefaultFooter() {
+  return (
+    <div>
+      <Box
+        sx={{
+          flexGrow: 1,
+          padding: 3,
+          backgroundColor: "#FFD8B1",
+          color: "#2F4F2F",
+        }}
+      >
+        <Grid container spacing={2}>
+          <Grid size={8}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <img src={Logo} alt="YoutMealLogo" style={{ width: 75 }} />
+              <p
+                style={{
+                  color: "#5D4037",
+                  fontWeight: "bold",
+                  textShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)",
+                }}
+              >
+                Malzemeler sizden, tarifler YourMeal’den!
+              </p>
+            </Box>
+          </Grid>
+          <Grid
+            size={4}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <p>
+              <Link to={""}>a</Link>
+            </p>
+            <p>
+              <Link to={""}>b</Link>
+            </p>
+            <p>
+              <Link to={""}>c</Link>
+            </p>
+            <p>
+              <Link to={""}>d</Link>
+            </p>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
+}
+
+export default DefaultFooter;
